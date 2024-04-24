d56d11caf6eeb2e4173c772f416e360d0f64eb34
Will Darling <Will_JD@outlook.com>
2024-04-24 17:07:43 +0100

    Lean simple data fetching from APIs

--- src/App.js
+++ src/App.js
@@ -1,59 +1,25 @@
 import './App.css';
-import { useState } from 'react';
-import {Task} from "./Task"
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
+  const [catFact, setCatFact] = useState("");
 
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
-  };
+  const fetchCatFact = () => {
+    Axios.get("https:/catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact)
+    });
+  }
 
-  const addTask = () => {
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      completed: false,
-    };
-    setTodoList([...todoList, task])
-  };
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
 
-  const removeTask = (id) => {
-    setTodoList(todoList.filter((task) => task.id !== id));
-  };
-
-  const completeTask = (id) => {
-    setTodoList(
-      todoList.map((task) => {
-        if (task.id === id) {
-          return {...task, completed: true};
-        } else {
-          return task
-        }
-      })
-    );
-  };
 
   return (
     <div className="App">
-      <div className="addTask">
-        <input onChange={handleChange}/>
-        <button onClick={addTask}>Add Task</button>
-      </div>
-      <div className="list">
-        {todoList.map((task) => {
-          return (
-            <Task 
-            taskName={task.taskName} 
-            id={task.id}
-            completed={task.completed}
-            removeTask={removeTask}
-            completeTask={completeTask}
-            />
-          ); 
-        })}
-      </div>
+      <button onClick={fetchCatFact}> Generate Cat Fact</button>
+      <p>{catFact}</p>
     </div>
   );
 }
--- src/Task.js
+++
@@ -1,9 +0,0 @@
-export const Task = (props) => {
-  return (
-    <div style={{backgroundColor: props.completed ? "green" : "white"}}>
-      <h2>{props.taskName}</h2>
-      <button onClick={() => props.completeTask(props.id)}>Complete</button>
-      <button onClick={() => props.removeTask(props.id)}>X</button>
-    </div>
-  );
-};

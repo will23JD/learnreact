6027ef876ab5e337ea4327be467218fa6b7b2bb3
Will Darling <Will_JD@outlook.com>
2024-04-24 17:41:04 +0100

    Fetching data from API's and passing info

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
+import { clear } from '@testing-library/user-event/dist/clear';
 import './App.css';
 import Axios from 'axios';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [catFact, setCatFact] = useState("");
-
-  const fetchCatFact = () => {
-    Axios.get("https:/catfact.ninja/fact").then((res) => {
-      setCatFact(res.data.fact)
-    });
-  }
-
-  useEffect(() => {
-    fetchCatFact();
-  }, []);
-
+  const [name, setName] = useState("")
+  const [averageAge, setAverageAge] = useState(null);
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+    setAverageAge(res.data)
+    })
+  };
 
   return (
     <div className="App">
-      <button onClick={fetchCatFact}> Generate Cat Fact</button>
-      <p>{catFact}</p>
+      <input 
+        placeholder="Ex. Will..." onChange={(event) => {
+        setName(event.target.value);
+        }}
+      />
+      <button onClick={fetchData}>Average Age</button>
+      <h1>Name: {averageAge?.name}</h1>
+      <h1>Average Age: {averageAge?.age}</h1>
+      <h1>Count: {averageAge?.count}</h1>
     </div>
   );
 }
-
 export default App;

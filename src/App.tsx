9e66afed8a79b5323edae7803f92f0a3ccd23dfd
Will Darling <Will_JD@outlook.com>
2024-05-01 17:22:51 +0100

    redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import './App.css';
-import {Person} from "./components/Person";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {Home} from "./pages/Home";
+import {Contact} from "./pages/Contact";
+import {Login} from "./pages/Login";
+import {Provider} from "react-redux";
+import { store } from "./store";
 
 function App() {
   return (
     <div className="App">
-      <Person
-        name="William"
-        email="William@will.com"
-        age={22}
-        isMarried={false}
-        friends={["Jacky", "Martin", "Wilson", "Jim"]}
-      />
+      <Provider store={store}>
+        <Router>
+          <Link to="/"> Home </Link>
+          <Link to="/login"> Login </Link>
+          <Link to="/contact"> Contact </Link>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      </Provider>
     </div>
   );
 }
